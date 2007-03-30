9f43338188b52d6b573f71ab91dff77f1f7874bc
esamynn <esamynn@9441417d-b421-0410-ba4c-c9a58315bafb>
2007-03-30 18:56:21 +0000

    -updated license header with a note about World of Warcraft's AddOn system
    AstrolabeMapMonitor.lua
    -added link to World Map Monitor documentation

--- trunk/Astrolabe.lua
+++ trunk/Astrolabe.lua
@@ -13,23 +13,29 @@ Description:
 	icons accurately on both the Minimap and the Worldmaps accurately
 	and maintain the accuracy of those positions.  
 
-License:
-
 Copyright (C) 2006-2007 James Carrothers
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+License:
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Note:
+	This library's source code is specifically designed to work with
+	World of Warcraft's interpreted AddOn system.  You have an implicit
+	licence to use this library with these facilities since that is its
+	designated purpose as per:
+	http://www.fsf.org/licensing/licenses/gpl-faq.html#InterpreterIncompat
 ]]
 
 -- WARNING!!!
