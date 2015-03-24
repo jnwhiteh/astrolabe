67a8a18bec6a281b7bef548720db390fa6d7d71c
MMOSimca <mmosimca@gmail.com>
2015-03-24 15:35:01 -0400

    Incremented revision number to 162.

--- Astrolabe.lua
+++ Astrolabe.lua
@@ -1,7 +1,5 @@
 --[[
 Name: Astrolabe
-Revision: $Rev$
-$Date$
 Author(s): Esamynn (esamynn at wowinterface.com)
 Inspired By: Gatherer by Norganna
              MapLibrary by Kristofer Karlsson (krka at kth.se)
@@ -42,7 +40,7 @@ Note:
 -- DO NOT MAKE CHANGES TO THIS LIBRARY WITHOUT FIRST CHANGING THE LIBRARY_VERSION_MAJOR
 -- STRING (to something unique) OR ELSE YOU MAY BREAK OTHER ADDONS THAT USE THIS LIBRARY!!!
 local LIBRARY_VERSION_MAJOR = "Astrolabe-1.0"
-local LIBRARY_VERSION_MINOR = tonumber(string.match("$Revision$", "(%d+)") or 1)
+local LIBRARY_VERSION_MINOR = 162
 
 if not DongleStub then error(LIBRARY_VERSION_MAJOR .. " requires DongleStub.") end
 if not DongleStub:IsNewerVersion(LIBRARY_VERSION_MAJOR, LIBRARY_VERSION_MINOR) then return end
